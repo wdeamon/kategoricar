9969cd962aa5300ca4bbb773179bbe3c2f68c814
Marin O <mobrano@racunarstvo.hr>
2015-10-25 17:27:13 +0000

    Using json to communicate, can self update and correct errors in database,logs erros in other.

--- client.py
+++ client.py
@@ -7,69 +7,81 @@ Scripta provjerava i izbacuje kategoriju i podrucni ured osigurane osobe!
 """
 import os
 from threading import Thread
-import pythoncom, pyHook
 import string
-import mechanize
-from bs4 import BeautifulSoup
 import datetime
-import sqlite3
 import socket
+import json
+
+import pythoncom
+import pyHook
+
 
 # variable ---------------------
 prijasnji = ""
 title, title1, keys, keylength = '', '', '', 9
 url = "http://www.hzzo-net.hr/statos_MBO.htm"
 podrucni = "004", "010", "018", "026", "034", "037", "040", "060", "065", "072", "078", "079", "080", "082", "083", "088", "090", "091", "100", "114"
-#variable end -----------------------
+rjecnik = {"mbo": "11111111", "kategorija": "0", "podrucni": "0", "osnova": "0", "brojPristupa": "0", "errorCode": "0",
+           "updateTime": "0", "vrstaZahtjeva": "1", "izvor": "0"}
+# variable end -----------------------
 
 
-def screen_show(event_code, mbo, kat=None, pod=None,osn=None, iz=0, update=None):
-    izvor =["Internet", "Baza"]
-    event_error_url= "Problem sa hzzo-om!! mbo: " + mbo + "\n\n"
-    event_error_mbo= "Pogresan MBO!! " + str(mbo)
-    event_success = "\n\n\n\n\nTrazeni MBO:    | " + str(mbo) + "\n\nKategorija:	| " + str(kat) + "\n\nPodrucni:	| " + str(pod) + "\n\n\n\n\n\nOsnova:         | " + str(osn) + "\n\nIzvor:          | " + izvor[iz]+ "\n\nzadnji update:	| " + str(update)
-    event_error_opcenito ="""MBO problem ?? \n\n\n\nvrijeme: """ + str(datetime.datetime.now())+ """
+def screen_show(event_code, data):
+    izvor = ["Baza", "Internet"]
+    event_error_url = "Problem sa hzzo-om!! mbo: " + data["mbo"] + "\n\n"
+    event_error_mbo = "Pogresan MBO/ nema osiguranje!! " + data["mbo"]
+    event_success = "\n\n\n\n\nTrazeni MBO:    | " + data["mbo"] + "\n\nKategorija:	| " + data[
+        "kategorija"] + "\n\nPodrucni:	| " + data["podrucni"] + "\n\n\n\n\n\nOsnova:         | " + data[
+                        "osnova"] + "\n\nIzvor:          | " + izvor[int(data["izvor"])] + "\n\nzadnji update:	| " + data["updateTime"]
+    event_error_opcenito = """MBO problem ?? \n\n\n\nvrijeme: """ + str(datetime.datetime.now()) + """
                             \n\nMBO nije tocan ili nema osiguranje... Provjeriti jos jednom MBO
-                            \n\n{{{  """ + str(mbo) + """   }}}}"""
-    event_dic = {"1":event_error_url, "2":event_error_mbo, "3":event_success, "4":event_error_opcenito}
+                            \n\n{{{  """ + str(data["mbo"]) + """   }}}}"""
+    event_dic = {"1": event_error_url, "3": event_error_mbo, "0": event_success, "2": event_error_opcenito}
     print event_dic[event_code]
 
-def obrada(value, mbo):
+
+def obrada(value):
     os.system("cls")
-    if(len(value[0])==1):
-        screen_show(value[0], mbo)
-    elif(len(value[0])>1):
-        podatak = string.split(value[0], "@")
-        screen_show(podatak[0], podatak[1], podatak[2], podatak[3], podatak[4], int(podatak[5]), podatak[6])
+    # ErrorCodes
+    #now every time we will have at least some data ... so we need to be careful
+
+    if (len(value["errorCode"]) != "0"):
+        screen_show(value["errorCode"], value)
+    elif (value["errorCode"] == "0"):
+        screen_show("0", value)
+
 
 def client_za_podatke(value):
     conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    conn.sendto(str(value), ("192.168.1.131", 8666))
-    data = conn.recvfrom(200)
+    conn.sendto(json.dumps(value), ("192.168.1.131", 8666))
+    data,addr = conn.recvfrom(2048)
     conn.close()
-    obrada(data, value)
-
+    obrada(json.loads(data))
 
 
 def keys_collecting(event):
-    global title, title1, keys, keylength, prijasnji
+    global title, title1, keys, keylength, prijasnji, rjecnik
     try:
         title = event.WindowName
         keys = keys + chr(event.Ascii)
         keys = keys.replace('', '[BS]')
         keys = keys.replace(' ?', '?')
-        if(keys[len(keys)-1]=="q"):
-            t = Thread(target=client_za_podatke, args=("00"+prijasnji,))
+        if (keys[len(keys) - 1] == "q"):
+            rjecnik["vrstaZahtjeva"] = "1"
+            rjecnik["mbo"] = prijasnji
+            t = Thread(target=client_za_podatke, args=(json.dumps(rjecnik),))
             t.start()
-            keys = keys[0:len(keys)-1]
-        #for ured in podrucni :
-        #    if ured == keys:
-        #        keys = ""
+            keys = keys[0:len(keys) - 1]
+    #for ured in podrucni :
+    #    if ured == keys:
+    #        keys = ""
         if unicode(keys).isnumeric() == False:
             keys = ""
         if len(keys) == keylength:
             prijasnji = keys
-            t = Thread(target=client_za_podatke, args=(keys,))
+            rjecnik["mbo"] = keys
+            rjecnik["vrstaZahtjeva"] = "0"
+            t = Thread(target=client_za_podatke, args=(json.dumps(rjecnik),))
             t.start()
             keys, title, title1 = '', '', ''
         return True
@@ -77,11 +89,14 @@ def keys_collecting(event):
         keys = ""
         print(inst)
         return True
-
-
-
-
-
+#1. greska servera
+#2. pogreska u podacima
+#3. krivi mbo/ nema podataka
+#0. nema greske
+#
+#0. normalni zahtjev
+#1. kompletna provjera
+#2. update
 hm = pyHook.HookManager()
 hm.KeyDown = keys_collecting
 hm.HookKeyboard()
--- server.py
+++ server.py
@@ -1,141 +1,293 @@
 # -*- coding: cp1250 -*-
 import socket
-from threading import Thread
-import sqlite3
-from bs4 import BeautifulSoup
-import datetime
-import os
-import mechanize
+import threading
 import string
+import datetime
+import time
+import sqlite3
+import os
+import json
+
+from bs4 import BeautifulSoup
+import mechanize
 
 
-#################################
-##konfiguracija
-#################################
+
+# ##############
+#configuration#
+#############################################
 url = "http://www.hzzo-net.hr/statos_MBO.htm"
+serverPort = 8666
+logPort = 8667
+updaterPort = 8668
 serverIP = "192.168.1.131"
-port = 8666
-#################################
+##############################################
 
-def dataObrada(mbo, kategorija, osnova, podrucni):
+def timer(timeout):
+    #time in secunds!!
+    now = time.time()
+    future = now + timeout
+    while (True):
+        if time.time() > future:
+            break
+
+
+def dataObrada(data):
     try:
         con = sqlite3.connect("kategoricar.db")
         cursor = con.cursor()
-        cursor.execute("select kategorija, mbo, counter from testTable where mbo = :Mbo ", {"Mbo":str(mbo)})
+        cursor.execute("select kategorija, mbo, counter from testTable where mbo = :Mbo ", {"Mbo": data["mbo"]})
         podaci = cursor.fetchall()
         if (podaci != []):
-            cursor.execute("update testTable set counter = :Counter where mbo= :Mbo ", {"Mbo":str(mbo), "Counter":podaci[0][2]+1})
+            cursor.execute("update testTable set counter = :Counter where mbo= :Mbo ",
+                           {"Mbo": data["mbo"], "Counter": podaci[0][2] + 1})
             con.commit()
         else:
-    #provjera na netu
-            cursor.execute("insert into testTable values(?,?,?,?,?,?)", (mbo, kategorija, datetime.datetime.now(), 0, osnova, podrucni))
+            #provjera na netu
+            cursor.execute("insert into testTable values(?,?,?,?,?,?)", (
+            data["mbo"], data["kategorija"], datetime.datetime.now(), 0, data["osnova"], data["podrucni"]))
             con.commit()
         con.close()
     except:
-          pass
+        pass
 
-def scraping(html, uneseni_mbo):
-    global keys
+
+def scraping(html, data):
     try:
-            soup = BeautifulSoup(html)
-            text = soup.get_text()
-            text = unicode(string.strip(text))
-            if not string.find(text, "Nema podataka") == -1:
-                    return 2
-                    keys= ""
-            else:
-                    kategorija = text[string.find(text,"Osnova:")-2:string.find(text,"Osnova:")]
-                    kategorija = kategorija[1]
-                    text=string.strip(text)
-                    osnova = text[string.find(text,"Osnova:")+10:string.find(text,"Osnova:")+15]
-                    podrucni = string.split(text, "DOPUNSKO OSIGURANJE")
-                    podrucni = podrucni[0][len(podrucni[0])-6:len(podrucni[0])]
-                    podrucni = string.strip(podrucni)
-                    if len(podrucni) == 1:
-                        podrucni = "00" + podrucni
-                    elif len(podrucni) == 2:
-                	podrucni = "0" + podrucni
-                    dataObrada(uneseni_mbo, kategorija, osnova, podrucni)
-                    data = str("@"+ uneseni_mbo + "@" + kategorija + "@" + podrucni + "@" + osnova + "@" + str(0) + "@" + str(datetime.datetime.now()))
-                    return "3"+data
-    except:
-		return 4
+        soup = BeautifulSoup(html)
+        text = soup.get_text()
+        text = unicode(string.strip(text))
+        if not string.find(text, "Nema podataka") == -1:
+            data["errorCode"] = "3"
+            return data
+        else:
+            kategorija = text[string.find(text, "Osnova:") - 2:string.find(text, "Osnova:")]
+            data["kategorija"] = kategorija[1]
+            text = string.strip(text)
+            data["osnova"] = text[string.find(text, "Osnova:") + 10:string.find(text, "Osnova:") + 15]
+            podrucni = string.split(text, "DOPUNSKO OSIGURANJE")
+            podrucni = podrucni[0][len(podrucni[0]) - 6:len(podrucni[0])]
+            data["podrucni"] = string.strip(podrucni)
+            if len(data["podrucni"]) == 1:
+                data["podrucni"] = "00" + str(data["podrucni"])
+            elif len(data["podrucni"]) == 2:
+                data["podrucni"] = "0" + str(data["podrucni"])
+            dataObrada(data)
+            data["izvor"] = "1"
+            data["updateTime"] = str(datetime.datetime.now())
+            return data
+    except Exception as inst:
+        data["errorCode"] = "1"
+        print str(inst)
+        return data
 
 
-
-
-def Ubazi(mbo):
+def ubazi(mbo):
     con = sqlite3.connect("kategoricar.db")
     cursor = con.cursor()
-    cursor.execute("select mbo, kategorija from testTable where mbo = :Mbo", {"Mbo":mbo})
+    cursor.execute("select mbo, kategorija from testTable where mbo = :Mbo", {"Mbo": mbo})
     data = cursor.fetchone()
+    con.close()
     try:
-        if(data == "[]" or data[1]== "Null"):
+        if (data == "[]" or data[1] == "Null"):
             return True
     except:
         return True
     return False
 
-def destilator(poruka):
-	global url
-	try:
-            if (len(poruka)==11):
-                poruka = poruka[2:11]
-                br = mechanize.Browser()
-                br.set_handle_robots(False) # ignore robots
-                br.open(url, timeout=8.0)
-                br.select_form(name="isktr")
-                br["upmbo"] = poruka
-                br["answer"]="11111"
-                res = br.submit()
-                content = res.read()
-                podatak = scraping(content, poruka)
-                br.close()
-                if len(str(podatak))==1:
-                    return podatak
-                podatak1= string.split(podatak, "@")
-                con = sqlite3.connect("kategoricar.db")
-                cursor = con.cursor()
-                cursor.execute("update testTable set kategorija=:Kate, podrucni=:Pon, osnova=:Osn, zadnjiUpdate=:Up where mbo= :Mbo ", {"Mbo":str(poruka), "Kate":podatak1[2], "Pon":podatak1[3], "Osn":podatak1[4], "Up":podatak1[6]})
-                con.commit()
-                return podatak
-            elif(Ubazi(poruka) ):
-                br = mechanize.Browser()
-                br.set_handle_robots(False) # ignore robots
-                br.open(url, timeout=8.0)
-                br.select_form(name="isktr")
-                br["upmbo"] = poruka
-                br["answer"]="11111"
-                res = br.submit()
-                content = res.read()
-                podatak = scraping(content, poruka)
-                br.close()
-                return podatak
-            else:
-                con = sqlite3.connect("kategoricar.db")
-                cursor = con.cursor()
-                cursor.execute("select kategorija, podrucni, osnova, zadnjiUpdate from testTable where mbo = :Mbo", {"Mbo":poruka})
-                data = cursor.fetchone()
-                data = str("@"+ poruka + "@" + data[0] + "@" + data[1] + "@" + data[2] + "@" + str(1) + "@" + data[3])
-                return "3"+data
 
-	except Exception as inst:
-            dataObrada(poruka, "Null", "Null", "Null")
-            print("Pogreska na serveru! MBO:" + poruka)
-            return 1
+def destilator(data):
+    global url
+    
+    try:
+        if (data["vrstaZahtjeva"] == "1" or data["vrstaZahtjeva"] == "2"):
+            br = mechanize.Browser()
+            br.set_handle_robots(False)  # ignore robots
+            br.open(url, timeout=8.0)
+            br.select_form(name="isktr")
+            br["upmbo"] = data["mbo"]
+            br["answer"] = "11111"
+            res = br.submit()
+            content = res.read()
+            data = scraping(content, data)
+            br.close()
+            if data["errorCode"] != "0":
+                if data["vrstaZahtjeva"] == "2":
+                    return data["errorCode"]
+                return data
+            con = sqlite3.connect("kategoricar.db")
+            cursor = con.cursor()
+            cursor.execute(
+                "update testTable set kategorija=:Kate, podrucni=:Pon, osnova=:Osn, zadnjiUpdate=:Up where mbo= :Mbo ",
+                {"Mbo": data["mbo"], "Kate": data["kategorija"], "Pon": data["podrucni"], "Osn": data["osnova"],
+                 "Up": data["updateTime"]})
+            con.commit()
+            if data["vrstaZahtjeva"] == "2":
+                return data["errorCode"]
+            return data
+        elif (ubazi(data["mbo"])):
+            br = mechanize.Browser()
+            br.set_handle_robots(False)  # ignore robots
+            br.open(url, timeout=8.0)
+            br.select_form(name="isktr")
+            br["upmbo"] = data["mbo"]
+            br["answer"] = "11111"
+            res = br.submit()
+            content = res.read()
+            podatak = scraping(content, data)
+            br.close()
+            return podatak
+        else:
+            con = sqlite3.connect("kategoricar.db")
+            cursor = con.cursor()
+            cursor.execute("select kategorija, podrucni, osnova, zadnjiUpdate from testTable where mbo = :Mbo",
+                           {"Mbo": data["mbo"]})
+            data_baza = cursor.fetchone()
+            data["kategorija"] = data_baza[0]
+            data["podrucni"] = data_baza[1]
+            data["osnova"] = data_baza[2]
+            data["izvor"] = "1"
+            data["updateTime"] = data_baza[3]
+            return data
+
+    except Exception as inst:
+        dataObrada(data)
+        #log :: progreska u obradi podataka
+        #print("Pogreska na serveru! MBO:" + poruka)
+        print  "ispis data"
+        print str(inst)
+        data = json.loads(data)
+        data["errorCode"] = "1"
+        if data["vrstaZahtjeva"] == "2":
+            return data["errorCode"]
+        return data
 
 
+def slanje(conn, addr, data):
+    conn.sendto(json.dumps(destilator(data)), addr)
 
-def slanje(conn,addr, data):
-	conn.sendto(str(destilator(data)), addr)
+
+def server_thread():
+    conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    conn.bind((serverIP, serverPort))
+    while 1:
+        data, addr = conn.recvfrom(2048)
+        data = json.loads(data)
+        try:
+            data["errorCode"] = 0
+        except:
+            data = json.loads(data)
+        #here goes code for logs!!
+        #print("Podaci(" + data["mbo"] + ") primljeni! IP:" + str(addr))
+        t = threading.Thread(target=slanje, args=(conn, addr, data))
+        t.start()
+    conn.close()
+
+
+def log_thread(value):
+    file_name = datetime.date.isoformat(datetime.date.today()) + ".txt"
+    datoteka = open(file_name, "a")
+    datoteka.write(value)
+    datoteka.close()
+       
+
+
+def updater_thread():
+    updater_con = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    updater_con.bind((serverIP, updaterPort))
+    rjecnik = {"mbo": "143520838", "kategorija": "A", "podrucni": "004", "osnova": "00112", "brojPristupa": "0",
+               "errorCode": "0", "updateTime": "14", "vrstaZahtjeva": "2", "izvor": "0"}
+    data = 0
+    a = []
+    while 1:
+        con = sqlite3.connect("kategoricar.db")
+        cursor = con.cursor()
+        cursor.execute("select  mbo  from testTable where kategorija='Null'")
+        podatak = cursor.fetchall()
+        for y in range(len(podatak)):
+            a.append(podatak[y][0])
+        if a != "[]":
+            while (a != "[]"):
+                for x in range(20):
+                    rjecnik["mbo"] = a[x]
+                    updater_con.sendto(json.dumps(rjecnik), (serverIP, serverPort))
+                    data, addr = updater_con.recvfrom(1024)
+                    timer(15)
+                    if data == "1":  # if there is server problems break and wait better times
+                        break
+                    if data == "3":  # if there is error with mbo try 3 more times, if there is everytime every delete it from database
+                        for y in range(2):
+                            updater_con.sendto(str(json.dumps(rjecnik)), (serverIP, serverPort))
+                            data, addr = updater_con.recvfrom(1024)
+                            timer(15)
+                            if data == "0":
+                                break
+                        if data == "1":
+                            break
+                        if data == "3":
+                            cursor.execute("delete from testTable where mbo=:Mbo", {"Mbo": a[x]})
+                            con.commit()
+
+                    data = 0
+                    del a[x]
+                    timer(15)
+                if data == "1":
+                    break
+
+        if data != "1":
+            cursor.execute(
+                "select mbo  from testTable where julianday('now') - julianday(zadnjiUpdate)>30 and kategorija = 'F' or kategorija = 'A' limit 260")
+            podatak = cursor.fetchall()
+            for y in range(len(podatak)):
+                a.append(podatak[y][0])
+            if a != "[]":
+                while (a != "[]"):
+                    for x in range(20):
+                        rjecnik["mbo"] = a[x]
+                        updater_con.sendto(str(json.dumps(rjecnik)), (serverIP, serverPort))
+                        data, addr = updater_con.recvfrom(1024)
+                        timer(15)
+                        if data == "1":  # if there is server problems break and wait better times
+                            break
+                        if data == "3":  # if there is error with mbo try 3 more times, if there is everytime error delete it from database
+                            for y in range(2):
+                                updater_con.sendto(str(json.dumps(rjecnik)), (serverIP, serverPort))
+                                data, addr = updater_con.recvfrom(1024)
+                                if data == "0":
+                                    break
+                                timer(15)
+                            if data == "1":
+                                break
+                            if data == "3":
+                                cursor.execute("delete from testTable where mbo=:Mbo", {"Mbo": a[x]})
+                                con.commit()
+
+                        data = 0
+                        del a[x]
+                        timer(15)
+                    if data == "1":
+                        break
+
+        timer(216000)
 
 
 if __name__ == "__main__":
-	conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	conn.bind((serverIP, port))
-	while True:
-		data, addr = conn.recvfrom(1024)
-		print("Podaci(" + data + ") primljeni! IP:" + str(addr))
-		t = Thread(target=slanje, args=(conn, addr, data))
-		t.start()
-	conn.close()
+    while 1:
+        #starting server thread
+        tekst = str(threading.enumerate())
+        if string.find(tekst, "server") == -1:
+            cunt = threading.Thread(target=server_thread, name="server")
+            cunt.start()
+
+
+        #starting log thread
+        #if string.find(tekst, "log") == -1:
+        #    cunt = threading.Thread(target=log_thread, name="log")
+        #    cunt.start()
+
+        #starting updater thread
+        #if string.find(tekst, "updater") == -1:
+        #    cunt = threading.Thread(target=updater_thread, name="updater")
+        #    cunt.start()
+        timer(30)
+#enter code for checks and so on
